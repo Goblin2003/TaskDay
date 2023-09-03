30f98bf7286d43dd7a52ebd9f5b29ba01ac04b36
MoreNip <Filip-lax2016@yandex.ru>
2023-09-03 12:24:52 +0300

    Добавьте файлы проекта.

---
+++ TaskDay/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TaskDay.Models;
+using TaskDay.Servises;
+
+namespace TaskDay
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly string PATH = $"{Environment.CurrentDirectory}\\todoDataList.json";
+        private BindingList<Taski> _todoDateList;
+
+        private FileIOServises _fileIOServises;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _fileIOServises = new FileIOServises(PATH);
+            try
+            {
+                _todoDateList = _fileIOServises.LoadDate();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                Close();
+            }
+            
+
+            taskDayList.ItemsSource = _todoDateList;
+            _todoDateList.ListChanged += _todoDateList_ListChanged;
+        }
+
+        private void _todoDateList_ListChanged(object sender, ListChangedEventArgs e)
+        {
+            if(e.ListChangedType == ListChangedType.ItemAdded || e.ListChangedType == ListChangedType.ItemDeleted || e.ListChangedType == ListChangedType.ItemChanged)
+            {
+                try
+                {
+                    _fileIOServises.SaveDate(sender);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    Close();
+                }
+            }
+            
+                
+        }
+    }
+}
---
+++ TaskDay/Servises/FileIOServises.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskDay.Models;
+
+namespace TaskDay.Servises
+{
+    internal class FileIOServises
+    {
+        private readonly string PATH;
+
+        public FileIOServises(string path)
+        {
+            PATH = path;
+        }
+        public BindingList<Taski> LoadDate() 
+        {
+            var fileExists = File.Exists(PATH);
+            if(!fileExists)
+            {
+                File.CreateText(PATH).Dispose();
+                return new BindingList<Taski>();
+            }
+            using (var stream = File.OpenText(PATH))
+            {
+                var fileText = stream.ReadToEnd();
+                return JsonConvert.DeserializeObject<BindingList<Taski>>(fileText);
+            }
+            
+        }
+        public void SaveDate(object todoDateList)
+        {
+            using (StreamWriter writer = File.CreateText(PATH))
+            {
+                string output = JsonConvert.SerializeObject(todoDateList);
+                writer.Write(output);
+            }
+        }
+    }
+}
